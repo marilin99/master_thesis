fe9dcd33543d1969789b293653086f01d8831be4
marilin99 <58047492+marilin99@users.noreply.github.com>
2023-05-09 04:59:38 +0300

    Update runner.py

--- Tool_setup/runner.py
+++ Tool_setup/runner.py
@@ -2,14 +2,18 @@
 import os 
 import subprocess 
 
-SRC_PATH_DM = r"G:\Automaatika\SEM_input"
-SRC_PATH_BAC = r"G:\Automaatika\CZI_input"
+# INSERT ABSOLUTE FOLDER PATH WHERE SEM IMAGES ARE INPUT
+SRC_PATH_DM = " "
+# INSERT ABSOLUTE FOLDER PATH WHERE CZI IMAGES ARE INPUT
+SRC_PATH_BAC = " "
 
 if len(os.listdir(SRC_PATH_DM)) > 0:
-    subprocess.run(["python", r"C:\Users\marilinm\Documents\py_scripts\diameter_pipeline_codes\main.py"])
+    # 2nd variable here is the absolute path to the main.py of the diameter measuring pipeline
+    subprocess.run(["python", "absolute-path-to-diameter-measuring-pipeline/main.py"])
     
 if len(os.listdir(SRC_PATH_BAC)) > 0:
-    subprocess.run(["python", r"C:\Users\marilinm\Documents\py_scripts\bacteria_pipeline_codes\main.py"])
+     # 2nd variable here is the absolute path to the main.py of the bacterial cell analysis pipeline
+    subprocess.run(["python", "absolute-path-to-bacterial-cell-analysis-pipeline/main.py"])
     
     
     
