4055c32619b5801442f7a3bf4b7614be7208af4b
marilin99 <marilin.moor@gmail.com>
2023-09-11 09:27:37 +0300

    Scale obtaining edited for unknown values for quick runs

--- Quick_runs/Diameter_measuring_pipeline/main.py
+++ Quick_runs/Diameter_measuring_pipeline/main.py
@@ -1,12 +1,11 @@
-### CODE FOR TESTING EXAMPLE SEM IMAGES when provided as arqument input ###
-
+#!/usr/bin/python3
+# CODE for checking a specific folder and running the diameter measuring pipeline #
 # libraries
 import cv2
 import numpy as np 
 import time 
 import os
 import re
-import sys 
 
 ## helper functions ##
 from classical_segmenter import classical_segment
@@ -14,102 +13,123 @@ from scale_obtain import scale_obtain
 from thinner import thinner, thinner_2k_5k
 from point_picker import point_picker
 from dm_finder import dm_finder
-from unet_pred import net_prediction
 
-PATH_1 = str(sys.argv[1])
-method = str(sys.argv[2])
-points = int(sys.argv[3])
+## adding data to the xlsx file ##
+from openpyxl import load_workbook
+from datetime import datetime
 
-## CLASSICAL segmentation block ##
-# otsu thresholding for 2k and 5k if specified in path
+first_part = "/run/user/1000/gvfs/smb-share:server=gaia.domenis.ut.ee,share=mvfa/"
+## To be processed SEMs folder on the Network Drive
+# ORIG_PATH = r"G:/Automaatika/SEM_input/"
 
-if method == "Classical":
-    if re.search("(_2k_|_5k_|2k)", PATH_1) != None:
+# ## moving files 
+# SRC_PATH = r"G:\Automaatika\SEM_input"
+# TARGET_PATH = r"G:\Automaatika\SEM_processed"
+# txt_TARGET_PATH = r"G:/Automaatika/SEM_processed/"
+
+# ## Excel workbook path ## 
+# filename = r"G:\Automaatika\SEM_analysis.xlsx"
+
+ORIG_PATH = first_part + "/Automaatika/SEM_input/"
+
+## moving files 
+SRC_PATH = first_part + r"Automaatika\SEM_input"
+TARGET_PATH = first_part + r"Automaatika\SEM_processed"
+txt_TARGET_PATH = first_part + r"Automaatika/SEM_processed/"
+
+## Excel workbook path ## 
+filename = first_part + "Automaatika\SEM_analysis.xlsx"
+
+FILES = os.listdir(ORIG_PATH) 
+#print(FILES)
+
+for f in FILES:
+
+    if f.endswith("png") or f.endswith("jpg") or f.endswith("tif"):
+
+        # dist values in pixels 
+        dist_bool = "No"
+        print("file name", f)
+
+        start_time = time.time()
+        PATH_1 = ORIG_PATH+f
+        # assuming an extension is 4 characters (including the ".")
+
+        core_name = f[:-4]
+
+
+        ## CLASSICAL segmentation ##
+        # otsu thresholding for 2k and 5k if specified in path
+        if re.search("(_2k_|_5k_|2k)", PATH_1) != None:
+            
+            segmented_im = cv2.threshold(cv2.imread(PATH_1, 0), 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+            dist, thinned = thinner_2k_5k(segmented_im)
         
-        segmented_im = cv2.threshold(cv2.imread(PATH_1, 0), 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-        dist, thinned = thinner_2k_5k(segmented_im)
+        else:
+            # classical segmentation for other magnifications
+            segmented_im = classical_segment(PATH_1)
 
-    else:
-        # classical segmentation for other magnifications
-        segmented_im = classical_segment(PATH_1)
-
-        dist, thinned = thinner(segmented_im)
-    
-### END OF CLASSICAL segmentation block ###
-
-## U-NET segmentation ## 
-
-elif method == "U-Net":
-    if re.search("(_2k_|_5k_|2k)", PATH_1) != None:
-        print("U-Net cannot handle 2k or 5k images right now, opting for Otsu's method instead")
-        segmented_im = cv2.threshold(cv2.imread(PATH_1, 0), 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-        dist, thinned = thinner_2k_5k(segmented_im)
+            dist, thinned = thinner(segmented_im)
+            
+        ### END OF CLASSICAL segmentation ###
         
-    else:
-        segmented_im = net_prediction(PATH_1)
-        dist, thinned = thinner(segmented_im)
+        # 100 measurements per image 
+        pt_s = point_picker(segmented_im, 100)
+        
+        # val, unit, px amount - from original image
+        scales = scale_obtain(PATH_1)
+        try:
+            if len(scales) == 3: 
+                if scales[1] == "um":
+                    nano_per_px = int(scales[0]) * 1000 / int(scales[-1])
+                # scale = nm 
+                elif scales[1] == "nm": 
+                    nano_per_px = int(scales[0]) / int(scales[-1])
 
-else:
-    print("OWNER NOTE: Make sure you have written the method correctly (either 'U-Net' or 'Classical') and that the arguments provided are in the correct order: abosolute file path, name of method, amount of points.")
+            else:
+                dist_bool = "Yes"
+                nano_per_px = 1
 
-
-### END OF U-NET segmentation block ##
-
-# 100 measurements per image 
-pt_s = point_picker(segmented_im, points)
-
-# val, unit, px amount - from original image
-scales = scale_obtain(PATH_1)
-
-if scales[1] == "um":
-    nano_per_px = int(scales[0]) * 1000 / int(scales[-1])
-# scale = nm 
-elif scales[1] == "nm": 
-    nano_per_px = int(scales[0]) / int(scales[-1])
-
-
-# leaving the lower part (scale included) in for now
-h,w = segmented_im.shape[:2]
-
-first_dm_s, first_excs, coords  = dm_finder(thinned, dist, segmented_im, pt_s, h,w,nano_per_px)
-
-# prints out diameters 
-print("Measured diameters", first_dm_s)
-
-# visualizing diameters 
-
-rgb_im = cv2.imread(PATH_1)
-
-start_color = (0, 0, 0)
-end_color = (255,0,0)  
-
-for combo in coords:
-    #combo = eval(combo)
-
-    start_point = (combo[1], combo[0])
-    mid_point = (combo[3], combo[2])
-
-    # testing the actual end point
-    end_point_x = (2 * mid_point[0]) - start_point[0]
-    end_point_y = (2 * mid_point[1]) - start_point[1]
-
-    end_point = (end_point_x, end_point_y)
-
-    # px dist
-    distance1 = np.sqrt((end_point[1] - start_point[1]) ** 2 + (end_point[0] - start_point[0]) ** 2)
-
-    cv2.line(rgb_im, start_point, mid_point, start_color, 2)
-    cv2.line(rgb_im, mid_point, end_point, (0,255,0), 2)
-
-    cv2.circle(rgb_im, mid_point, 3, end_color, -1)
+        except Exception: 
+            # adding a file for scale exception
+            nano_per_px = 1
+            dist_bool = "Yes"
+            #with open(f"{txt_TARGET_PATH}{core_name}.txt", "w+") as file_ex:
+            #    file_ex.write("Scaling off, check the image, adding distance in pixels for now")
+            #continue
 
 
-image = cv2.cvtColor(rgb_im, cv2.COLOR_RGB2BGR)
+        # leaving the lower part (scale included) in for now
+        h,w = segmented_im.shape[:2]
 
-cv2.imshow(f'Original image with diameter lines', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        first_dm_s, first_excs, coords  = dm_finder(thinned, dist, segmented_im, pt_s, h,w,nano_per_px) #[:3]
 
-#####
+        ### saving values to the XLSX file ###
+        sheet_name = datetime.now().strftime("%B") +" "+datetime.now().strftime("%Y")
+
+        new_row = [core_name, str(first_dm_s), np.mean(first_dm_s), np.std(first_dm_s), np.median(first_dm_s), time.time() - start_time, dist_bool]
+
+        wb = load_workbook(filename)
+
+        if sheet_name in wb.sheetnames:
+            ws = wb.worksheets[-1] # select last worksheet
+        else:
+            wb.create_sheet(sheet_name)
+            headers_row =  ["File path", "Diameter measures (nm)", "Mean (nm)", "Standard deviation (nm)", "Median (nm)", "Runtime (s)", "Values in pixels"]
+            ws = wb.worksheets[-1]
+            ws.append(headers_row)
+
+        ws.append(new_row)
+        wb.save(filename)
+        wb.close()
+
+        start_time = time.time()
+
+        ## moving the file to the target path ## 
+
+        os.rename(os.path.join(SRC_PATH, f), os.path.join(TARGET_PATH, f))
+
+
+    #####
     
 
--- Quick_runs/Diameter_measuring_pipeline/scale_obtain.py
+++ Quick_runs/Diameter_measuring_pipeline/scale_obtain.py
@@ -1,4 +1,5 @@
 # CODE for obtaining the scale from the SEM image #
+#!/usr/bin/python3
 # libraries
 import numpy as np
 import cv2
@@ -13,12 +14,12 @@ value_unit_scale = []
 # list of units
 pot_units = ["nm", "um"]
 # pot scale values - should be updated if some new scales used!
-pot_values = ["10", "1", "2", "400", "200", "3"]
+pot_values = ["1", "2", "3", "4", "10", "20", "100", "200", "400"]
 
-# INSERT YOUR tesseract.exe PATH here in case of having an error
+# INSERT YOUR tesseract.exe PATH here in case having an error
 # make sure tesseract OCR is installed too
-# pytesseract.pytesseract.tesseract_cmd = 'tesseract.exe PATH'
-
+# uncomment for lab pc
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
 def scale_obtain(file):
@@ -29,21 +30,33 @@ def scale_obtain(file):
 
     img = cv2.imread(file, 0)
 
-    counter = []
-    value_unit_scale = []
+    counter, value_unit_scale, four_contours = [], [], []
 
     ###########################################################################
     # number and unit obtaining
 
     thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV)[1]
 
-
     result = img.copy()
     contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #print(contours)
+    
+    #cv2.imshow("thresh", np.uint8(thresh))
 
     contours = contours[0] if len(contours) == 2 else contours[1]
-    big_contour = max(contours, key=cv2.contourArea)
+    #print(contours)
+
+    
+    for contour in contours:
+        if len(contour) == 4:
+            four_contours.append(contour)
+    
+    #print(four_contours)
+    try:
+        big_contour = max(four_contours, key=cv2.contourArea)
+    except: 
+        big_contour = max(contours, key=cv2.contourArea)
+    
+    #print(big_contour)
 
     # big_contour struc: [a][0][b], where a is the corner idx (runs from upper left + ↓ and upper right + ↓), b is either y [0] or x [1] coord
     # initial flattened big contour list y1 x1 y2 x2 etc.
@@ -55,9 +68,12 @@ def scale_obtain(file):
     if len(big_contour) > 4:
         big_contour = np.array([ [[min(y_coords), max(x_coords)]], [[min(y_coords), min(x_coords)]], [[max(y_coords), min(x_coords)]], [[max(y_coords), max(x_coords)]] ])
 
-    cv2.drawContours(result, [big_contour], 0, (255,255,255), 1)
+    cv2.drawContours(result, [big_contour], 0, (255,255,255), 5)
 
-    # 1px height cross-section
+    #cv2.imshow("contours", np.uint8(result))
+
+
+    # # 1px height cross-section
     cross = img[int( (min(x_coords)+ max(x_coords)) //2 ): int( (min(x_coords)+ max(x_coords)) //2 )+1, min(y_coords):max(y_coords)+5]
 
     ar_z = np.insert(np.nonzero(cross), 0, 0)
@@ -73,10 +89,13 @@ def scale_obtain(file):
     # extracting unit
     unit = img[min(x_coords):max(x_coords), min(y_coords) + cutting_idx:max(y_coords)]
     unit = np.pad(unit, pad_width = [(1, 1),(1, 1)], mode = "constant")
+    #cv2.imshow("unit", np.uint8(unit))
 
     # extracting number
     number = img[min(x_coords):max(x_coords), min(y_coords): min(y_coords) + cutting_idx]
     number = np.pad(number, pad_width = [(1, 1),(1, 1)], mode = "constant")
+
+    #cv2.imshow("number", np.uint8(number))
 
     # segmentation technique - one line with many possible char-s
     # https://github.com/madmaze/pytesseract
@@ -86,19 +105,37 @@ def scale_obtain(file):
 
     try:
         for val in range(10,30, 1):
-            detected_nr = str(pytesseract.image_to_string(cv2.resize(number, (val, val)), config =custom_config, timeout = 2)).split("y\n\x0c")[0].strip() # Timeout after 2 seconds
+            detected_nr = str(pytesseract.image_to_string(cv2.resize(number, (val, val)), config =custom_config, timeout = 5)).split("y\n\x0c")[0].strip() # Timeout after 2 seconds
             # give nr options
+            #print("numero", detected_nr)
             if detected_nr in pot_values:
                 value_unit_scale.append(int(detected_nr))
+                # extra list in case more than one value is detected 
                 counter.append(1)
 
-            detected_unit = str(pytesseract.image_to_string(cv2.resize(unit, (val, val)), config =custom_config, timeout = 2)).split("y\n\x0c")[0].strip() # Timeout after 2 seconds
+            detected_unit = str(pytesseract.image_to_string(cv2.resize(unit, (val, val)), config =custom_config, timeout = 5)).split("y\n\x0c")[0].strip() # Timeout after 2 seconds
+            #print(detected_unit)
             if detected_unit in pot_units:
                 value_unit_scale.append(detected_unit)
                 counter.append(2)
 
+            #print(counter)
+            #print(value_unit_scale)
             if len(np.unique(counter)) == 2:
-                value_unit_scale = list(np.unique(value_unit_scale))
+                state_counts = np.unique(value_unit_scale, return_counts=True)[1]
+                # checking for unique values and occurring once 
+                if np.in1d(state_counts, 1).all():
+                    value_unit_scale = list(np.unique(value_unit_scale))
+                else:
+                    max_num = np.unique(value_unit_scale)[np.argmax(state_counts)]
+                    #val = np.unique(value_unit_scale)[max_idx]
+                    #fin_unit = value_unit_scale[-1]
+                    # adding only the value
+                    if isinstance(max_num, int):
+                        value_unit_scale = [max_num, detected_unit]
+                    else:
+                        value_unit_scale = list(np.unique(value_unit_scale))
+
                 break
 
 
@@ -114,8 +151,23 @@ def scale_obtain(file):
     try:
         scale_length = np.max(np.nonzero(th2)[1]) - np.min(np.nonzero(th2)[1])
         value_unit_scale.append(scale_length)
-
+        
         return value_unit_scale
     # in case no unit or number is found
     except:
         return None
+    
+    
+#ORIG_PATH = "/run/user/1000/gvfs/smb-share:server=gaia.domenis.ut.ee,share=mvfa/Automaatika/SEM_processed/"
+ORIG_PATH = "/home/marilin/Documents/ESP/master_thesis/Data/SEM_images/"
+FILES = os.listdir(ORIG_PATH)
+#FILES = ["7gel02kit 10kx 2.tif"]
+
+for file in FILES:
+    print(file)
+    print(scale_obtain(ORIG_PATH+file))
+
+
+# # scale_obtain("/run/user/1000/gvfs/smb-share:server=gaia.domenis.ut.ee,share=mvfa/Automaatika/SEM_input/3gel 02kit hfip 10k 1.tif")
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
