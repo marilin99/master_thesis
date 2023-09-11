d34df97d082fb66b60bbeef63579e482f05248de
marilin99 <marilin.moor@gmail.com>
2023-09-11 10:09:19 +0300

    Quick runs modified based on tool setup edits

--- Quick_runs/Diameter_measuring_pipeline/scale_obtain.py
+++ Quick_runs/Diameter_measuring_pipeline/scale_obtain.py
@@ -14,7 +14,7 @@ value_unit_scale = []
 # list of units
 pot_units = ["nm", "um"]
 # pot scale values - should be updated if some new scales used!
-pot_values = ["1", "2", "3", "4", "10", "20", "100", "200", "400"]
+pot_values = ["1", "2", "3", "4", "10", "20", "30", "100", "200", "400"]
 
 # INSERT YOUR tesseract.exe PATH here in case having an error
 # make sure tesseract OCR is installed too
@@ -159,13 +159,13 @@ def scale_obtain(file):
     
     
 #ORIG_PATH = "/run/user/1000/gvfs/smb-share:server=gaia.domenis.ut.ee,share=mvfa/Automaatika/SEM_processed/"
-ORIG_PATH = "/home/marilin/Documents/ESP/master_thesis/Data/SEM_images/"
-FILES = os.listdir(ORIG_PATH)
+#ORIG_PATH = "/home/marilin/Documents/ESP/master_thesis/Data/SEM_images/"
+#FILES = os.listdir(ORIG_PATH)
 #FILES = ["7gel02kit 10kx 2.tif"]
 
-for file in FILES:
-    print(file)
-    print(scale_obtain(ORIG_PATH+file))
+#for file in FILES:
+#    print(file)
+#    print(scale_obtain(ORIG_PATH+file))
 
 
 # # scale_obtain("/run/user/1000/gvfs/smb-share:server=gaia.domenis.ut.ee,share=mvfa/Automaatika/SEM_input/3gel 02kit hfip 10k 1.tif")
